01e22ad47db241943bf8c34877ef60ae30c9448d
EElectriX <saptarshimondal1002@gmail.com>
2025-08-01 11:21:50 +0530

    adding loading component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,23 @@ import ProfileCard from './components/profilecard/ProfileCard';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
 import ProfessionalFooter from './components/ProfessionalFooter';
+import LoadingPage from './components/LoadingPage';
 import './App.css'
 // import Hero from './components/Hero/Hero';
 // import Nav from './components/Nav/Nav';
 // import ThreeColumns from './components/ThreeColumns';
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const handleLoadComplete = () => {
+    setIsLoading(false);
+  };
+
+  if (isLoading) {
+    return <LoadingPage onLoadComplete={handleLoadComplete} />;
+  }
+
+
 
   return (
     // <Router>
